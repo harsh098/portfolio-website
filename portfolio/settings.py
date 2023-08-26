44d150644decb9a6d27558cccf05759b30c49126
Harsh Mishra <hmisraji07@gmail.com>
2023-08-26 22:54:53 +0530

    added host

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'hmx86.dev', 'portfoliohmx.azurewebsites.net']
-
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'hmx86.tech']
 
 # Application definition
 
