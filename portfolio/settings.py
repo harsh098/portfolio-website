ed62a62496e8c1df8d936b5fa5172212e112d6b0
Harsh Mishra <23445112+harsh098@users.noreply.github.com>
2024-09-02 01:33:00 +0530

    Change Domain for CORS

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'hmx86.tech', 'hmx86.cloud']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://hmx86.cloud']
 
 # Application definition
 
